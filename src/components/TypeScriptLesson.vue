1edba41a62d6bc4aa374a9d68e33f59654e98fbc
amandine lescieux (ext) <amandine.lescieux@FR-25B2ML3.groupinfra.com>
2023-09-25 10:32:17 +0200

    TypeScript Lesson

---
+++ src/components/TypeScriptLesson.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+let name = "Amandine";
+let age = 30;
+let sex = true;
+let occupation: string;
+
+occupation = 'software developer'
+
+age = 26
+// sex = 'female'
+
+const arrayOfNumbers: (string | number)[] = []
+
+arrayOfNumbers.push(name)
+arrayOfNumbers.push(age)
+
+// const employee: {
+//   name: string,
+//   age: number,
+//   sex: boolean,
+//   occupation?: string
+// } = {
+//   name: 'Amandine',
+//   age: 30,
+//   sex: true
+// }
+
+interface Employee {
+  name: string,
+  age: number,
+  sex: GENDER,
+  occupation?: string
+}
+
+enum GENDER {
+  FEMALE, MALE, NONBINARY
+}
+const employee: Employee = {
+  name: "Amandine",
+  age: 30,
+  sex: GENDER.FEMALE
+}
+
+employee.occupation = 'Software Developer'
+employee.sex = GENDER.MALE
+employee.sex = GENDER.NONBINARY
+</script>
+
+<template>
+  <main></main>
+</template>
+
+<style scoped>
+
+</style>
