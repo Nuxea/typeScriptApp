e2805ce45c9392c7b06c7e64e0fad630e4cb8e60
amandine lescieux (ext) <amandine.lescieux@FR-25B2ML3.groupinfra.com>
2023-09-25 10:48:56 +0200

    Types with State

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,42 @@
 <script setup lang="ts">
+import {ref} from "vue";
 
+enum GENDER {
+  MALE, FEMALE
+}
+
+const name = ref('')
+const gender = ref(GENDER.MALE)
 </script>
 
 <template>
-  <main></main>
+  <main>
+    <div class="container">
+      <h1>People invited to my party</h1>
+      <div class="input-container">
+        <input v-model="name" type="text" placeholder="Name...">
+        <select v-model="gender">
+          <option :value="GENDER.MALE">Male</option>
+          <option :value="GENDER.FEMALE">Female</option>
+        </select>
+      </div>
+    </div>
+  </main>
 </template>
 
 <style scoped>
+main {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
+input, select {
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 5px;
+  border: none;
+  border-radius: 5px;
+}
 </style>
